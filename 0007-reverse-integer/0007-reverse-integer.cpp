a4b850e6eb9d5dbf8b8da23bfd2f5c07a79f7bc6
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-04-09 17:29:21 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (52.04%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long r = 0;
+        while(x) {
+            r = r*10 + x%10;
+            x /= 10;
+        }
+        if(r < INT_MIN || r > INT_MAX) return 0;
+        return int(r);
+    }
+};
