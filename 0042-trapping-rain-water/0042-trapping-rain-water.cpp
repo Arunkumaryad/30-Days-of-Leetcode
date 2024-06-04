33930cd0fa5bce56c07bf78afa27a2360d6a964b
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-06-04 17:20:51 +0530

    Time: 7 ms (87.72%), Space: 22.4 MB (64.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
-        while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+    int solve(vector<int>& h) {
+        int n = h.size();
+        int start = 0;
+        int end = n-1;
+        int leftMax = INT_MIN;
+        int rightMax = INT_MIN;
+        int ans = 0;
+        while(start < end) {
+            leftMax = max(leftMax,h[start]);
+            rightMax = max(rightMax,h[end]);
+            if(rightMax > leftMax) {
+                ans += leftMax-h[start];
+                start++;
+            }
+            else{
+                ans += rightMax-h[end];
+                end--;
+            }
         }
+        return ans;
+    }
+    int trap(vector<int>& h) {
+        int ans = solve(h);
         return ans;
     }
 };
