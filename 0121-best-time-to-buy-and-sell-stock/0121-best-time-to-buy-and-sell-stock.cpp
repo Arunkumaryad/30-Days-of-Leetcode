00acd7998615763e04b13fef6c80d866ee121ce4
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-08-12 17:41:03 +0530

    Time: 96 ms (16.98%), Space: 96.2 MB (16.98%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int minPrice = INT_MAX;
         int maxi = 0;
         for(int i=0;i<n;i++) {
-            minPrice = min(minPrice, prices[i]);
+            minPrice = min(minPrice,prices[i]);
             int diff = prices[i]-minPrice;
             maxi = max(maxi,diff);
         }
