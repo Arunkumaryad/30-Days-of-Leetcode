19cf1e6bef8ca75f75c917732cb8f79f91902357
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-09-17 23:34:48 +0530

    Time: 0 ms (100%), Space: 7.6 MB (12.8%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -20,9 +20,21 @@ public:
         dp[n] = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
         return dp[n];
     }
-    int fib(int n) {
+    int solveUsingTabulation(int n) {
         vector<int>dp(n+1,-1);
-        int ans = solveUsingMem(n, dp);
+        dp[0] = 0;
+        if(n == 0) {
+            return dp[0];
+        }
+        dp[1] = 1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    int fib(int n) {
+        // vector<int>dp(n+1,-1);
+        int ans = solveUsingTabulation(n);
         return ans;
     }
 };
