62c2e25d3948ea27ff9e24273eca95506ba0f311
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-11-03 18:27:12 +0530

    Time: 8 ms (14.73%), Space: 10.5 MB (13.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long rev = 0;
-        int temp = x;
-        while(temp) {
-            int digit = temp % 10;
-            rev = rev*10 + digit;
-            temp /= 10;
+        string str = to_string(x);
+        int n = str.size();
+        int start = 0;
+        int end = n-1;
+        while(start <= end) {
+            if(str[start] != str[end]) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        if(rev == abs(x)) {
-            return true;
-        }
-        return false;
+        return true;
     }
 };
