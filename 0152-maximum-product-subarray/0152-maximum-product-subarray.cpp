539fcceeb6bf3300c75cb2eaacc74007b844a171
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2025-03-11 15:57:39 +0530

    Time: 0 ms (100%), Space: 17.7 MB (84.41%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,12 +4,14 @@ public:
         int ans = INT_MIN;
         int n = nums.size();
         int prod = 1;
+        //from left to right
         for(int i=0;i<n;i++) {
             prod *= nums[i];
             ans = max(ans,prod);
             if(prod == 0) prod = 1;
         }
         prod = 1;
+        //from right to left
         for(int i = n-1;i>=0;i--) {
             prod *= nums[i];
             ans = max(ans,prod);
