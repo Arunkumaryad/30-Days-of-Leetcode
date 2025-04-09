d218fb54bb759d031000f7870e00c5c783d145e8
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2025-04-09 21:54:21 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.87%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int firstOccurrence(vector<int>& nums, int target) {
+    int firstOccurance(vector<int>& nums, int target) {
         int n = nums.size();
+        int ans = -1;
         int start = 0;
         int end = n-1;
-        int ans = -1;
         int mid = start + (end - start) / 2;
         while(start <= end) {
             if(nums[mid] == target) {
@@ -14,18 +14,19 @@ public:
             else if(nums[mid] < target) {
                 start = mid + 1;
             }
-            else {
+            else { 
                 end = mid - 1;
             }
             mid = start + (end - start) / 2;
         }
         return ans;
     }
-    int lastOccurrence(vector<int>& nums, int target) {
+
+    int lastOccurance(vector<int>& nums, int target) {
         int n = nums.size();
+        int ans = -1;
         int start = 0;
         int end = n-1;
-        int ans = -1;
         int mid = start + (end - start) / 2;
         while(start <= end) {
             if(nums[mid] == target) {
@@ -35,17 +36,18 @@ public:
             else if(nums[mid] < target) {
                 start = mid + 1;
             }
-            else {
+            else { 
                 end = mid - 1;
             }
             mid = start + (end - start) / 2;
         }
         return ans;
     }
+
     vector<int> searchRange(vector<int>& nums, int target) {
         pair<int,int>p;
-        p.first = firstOccurrence(nums,target);
-        p.second = lastOccurrence(nums,target);
-        return {p.first, p.second};
+        p.first = firstOccurance(nums,target);
+        p.second = lastOccurance(nums,target);
+        return {p.first,p.second};
     }
 };
