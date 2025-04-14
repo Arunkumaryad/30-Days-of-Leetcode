b992ecfd6ee2b98ff745cab1588819d4ed441496
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2025-04-14 14:45:41 +0530

    Time: 3 ms (54.53%), Space: 9.9 MB (12.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>mp;
-        for(auto it : s) {
-            mp[it]++;
+        unordered_map<char, int> mp;
+        for(auto str : s) {
+            mp[str]++;
         }
-        for(auto it : t) {
-            mp[it]--;
+        for(auto str : t) {
+            mp[str]--;
         }
         for(auto it : mp) {
             if(it.second != 0) {
